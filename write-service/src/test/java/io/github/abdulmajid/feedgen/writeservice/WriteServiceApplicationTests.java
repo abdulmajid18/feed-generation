88a8071b972c85597179eedbd1caebac959ab03d
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-22 23:18:32 +0000

    write service

---
+++ write-service/src/test/java/io/github/abdulmajid/feedgen/writeservice/WriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abdulmajid.feedgen.writeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

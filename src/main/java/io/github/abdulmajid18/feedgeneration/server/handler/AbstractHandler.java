b0dc8668c97b8f22d41fae21ff0c3f4788db8b19
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-23 13:35:53 +0000

    add service, dtos and impl

--- src/main/java/io/github/abdulmajid18/feedgeneration/server/handler/AbstractHandler.java
+++ src/main/java/io/github/abdulmajid18/feedgeneration/server/handler/AbstractHandler.java
@@ -3,9 +3,33 @@ package io.github.abdulmajid18.feedgeneration.server.handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 public abstract class AbstractHandler implements HttpHandler {
 
-    protected void sendResponse(HttpExchange httpExchange) {
+    protected void sendResponse(HttpExchange exchange, String response, int code) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(code, response.length());
+
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+    }
+
+       protected String readRequest(HttpExchange exchange) throws IOException {
+        StringBuilder requestBody = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(exchange.getRequestBody())
+        )){
+            String line;
+            while ((line = br.readLine()) != null){
+                requestBody.append(line).append("\n");
+            }
+            return requestBody  .toString().trim();
+        }
 
     }
 }
